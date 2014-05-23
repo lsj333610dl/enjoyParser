482a51e8a7d057a8ee905fe41ffae49c03724904
lsj333610dl <lsj333610dl@naver.com>
2014-05-23 15:09:34 +0900

    수정수정

--- app.js
+++ app.js
@@ -15,9 +15,9 @@ var url = require('url');
 
 var wordpress = require('wordpress');
 var wp = wordpress.createClient({
-            "url": 'http://enjoylimit.tv', 
-            "username": 'enjoylimit', 
-            "password": '963852dl' 
+    "url": 'http://enjoylimit.tv', 
+    "username": 'enjoylimit', 
+    "password": '963852dl' 
 });
 
 var fs = require("fs");
@@ -31,12 +31,10 @@ fs.readFile('./posted.enjoy', 'utf8', function(err, data) {
   
     if (!err) {
         oPoosted = JSON.parse(data);
-        console.log("로그 로드완료.");
+        console.log("로그 로드 완료.");
     };
 });
 
-
-var mysql = require('mysql');
 
 
 var app = express();
@@ -71,145 +69,16 @@ app.use('/shopagent', function(req, res) {
     });
 });
 
-// app.use('/tvko',function(req,res){
-//     // console.log(req.query);
-
-//     var tvkoUrl = req.query.url;
-
-//     request(tvkoUrl,function(error,response,body){
-//         if (!error && response.statusCode ==200) {
-
-//             var window = jsdom.jsdom(body).parentWindow;
-
-//             jsdom.jQueryify(window, "http://code.jquery.com/jquery.js",function(){
-//                 var contentHtml = window.$('div.video').html();
-//                 var sTitle = window.$('.post-top').find('a:first').text();
-
-//                 // console.log(contentHtml);
-
-//                 wp.newPost({title:sTitle,status:'publish',content:contentHtml,author:1},function(){console.log(arguments)});
-//                 res.render('parser',{title:sTitle,contents:contentHtml});
-
-//             });
-//         };
-//     });
-// });
-
-
-// app.use('/tvkoList',function(req,res){
-
-//     var tvkoUrl = 'http://tvko.us/page/1/';
-
-//     request(tvkoUrl,function(error,response,body){
-//         if (!error && response.statusCode ==200) {
-
-//             var window = jsdom.jsdom(body).parentWindow;
-
-//             jsdom.jQueryify(window, "http://code.jquery.com/jquery.js",function(){
-
-//                 var contentHtml = window.$('div.col-left').html();
-
-//                 // window.$('div.col-left').find('a').each(function(){
-//                 //     console.log($(this).text());
-//                 // });
-
-//                 console.log(window.$('div.col-left').find('div.inside').length);
-
-//                 // res.render('parser',{title:sTitle,contents:contentHtml});
-
-//             });
-//         };
-//     });
-
-// });
-
-function getVideo(url){
-
-    if (aPostedURL.indexOf(url)>(-1)) {
-        console.log('중복');
-        
-        // request(url,function(error,response,body){
-        //     if (!error && response.statusCode ==200) {
-
-        //         var window = jsdom.jsdom(body).parentWindow;
-
-        //         jsdom.jQueryify(window, "http://code.jquery.com/jquery.js",function(){
-        //             var contentHtml = window.$('div.video').html();
-        //             var sTitle = window.$('.post-top').find('a:first').text();
-
-        //             // console.log(contentHtml);
-        //             console.log('url : ',aPostedURL[aPostedURL.indexOf(url)],'id : ',aPostID[aPostedURL.indexOf(url)]);
-        //             wp.editPost(aPostID[aPostedURL.indexOf(url)],{title:sTitle,status:'publish',content:contentHtml,author:1},function(){
-        //                 console.log(arguments);
-        //                 writeLog();
-        //             });
-
-        //         });
-        //     };
-        // });
 
 
 
-        return;
-    };
-
-    request(url,function(error,response,body){
-        if (!error && response.statusCode ==200) {
-            console.log('getVideo성공');
-            var window = jsdom.jsdom(body).parentWindow;
-
-            jsdom.jQueryify(window, "http://code.jquery.com/jquery.js",function(){
-                var contentHtml = window.$('div.video').html();
-                var sTitle = window.$('.post-top').find('a:first').text();
-
-                // console.log(contentHtml);
-
-                wp.newPost({title:sTitle,status:'publish',content:contentHtml,author:1},function(){
-                    // aPostedURL.push(url);
-                    // aPostID.push(arguments['1']);
-                    // console.log('url : ',aPostedURL[aPostedURL.indexOf(url)],'id : ',aPostID[aPostedURL.indexOf(url)]);
-                    console.log(arguments);
-                    writeLog();
-                });
-
-            });
-        };
+function writeLog(){
+    var filedata = JSON.stringify(oPoosted);
+    fs.writeFile('./posted.enjoy', filedata, function(err) {
+        if(err) throw err;
+        console.log('Log File write completed');
     });
-};
-
-
-function test(page){
-    console.log(page,'시작');
-    // for(var i=page;i>0;i--){
-        var tvkoUrl = 'http://tvko.us/page/'+page;
-
-        request(tvkoUrl,function(error,response,body){
-            if (!error && response.statusCode ==200) {
-                console.log('req성공');
-                var window = jsdom.jsdom(body).parentWindow;
-
-                jsdom.jQueryify(window, "http://code.jquery.com/jquery.js",function(){
-
-                    var contentHtml = window.$('div.col-left').html();
-
-                    window.$('div.col-left').find('div.inside > a').each(function(){
-                        var url = window.$(this).attr('href');
-                        getVideo(url);
-                        // console.log('제목 : ',window.$(this).attr('title'),'\n주소 : ',window.$(this).attr('href'));
-                    });
-
-
-                    // res.render('parser',{title:sTitle,contents:contentHtml});
-
-                });
-            };
-        });
-
-    // }//for
-    
 }
-
-
 
 
 
@@ -217,52 +86,8 @@ function test(page){
 
 function getVideo2(url){
 
-    // if (aPostedURL.indexOf(url)>(-1)) {
-    //     console.log('중복');
-
-    //     request(url,function(error,response,body){
-    //         if (!error && response.statusCode ==200) {
-
-    //             var window = jsdom.jsdom(body).parentWindow;
-
-    //             jsdom.jQueryify(window, "http://code.jquery.com/jquery.js",function(){
-    //                 var contentHtml = window.$('div.video').html();
-    //                 var sTitle = window.$('.post-top').find('a:first').text();
-
-    //                 // console.log(contentHtml);
-    //                 console.log('url : ',aPostedURL[aPostedURL.indexOf(url)],'id : ',aPostID[aPostedURL.indexOf(url)]);
-    //                 wp.editPost(aPostID[aPostedURL.indexOf(url)],{title:sTitle,status:'publish',content:contentHtml,author:1},function(){
-    //                     console.log(arguments);
-    //                 });
-
-    //             });
-    //         };
-    //     });
-
-
-
-    //     return;
-    // };
-
     if (oPoosted[url]) {
         console.log('중투더복');
-
-        jsdom.env({
-            url: url,
-            src: [jquery],
-            done: function (errors, window){
-                var $ = window.$;
-                var contentHtml = $('div.video').html();
-                var sTitle = $('.post-top').find('a:first').text();
-
-                // console.log(contentHtml);
-
-                wp.editPost(oPoosted[url],{title:sTitle,status:'publish',content:contentHtml,author:1},function(){
-                    console.log(arguments);
-                });
-            }
-        });
-
         return;
     };
 
@@ -275,14 +100,10 @@ function getVideo2(url){
             var contentHtml = $('div.video').html();
             var sTitle = $('.post-top').find('a:first').text();
 
-            // console.log(contentHtml);
-
             wp.newPost({title:sTitle,status:'publish',content:contentHtml,author:1},function(){
                 oPoosted[url] = arguments['1'];
-                // aPostedURL.push(url);
-                // aPostID.push(arguments['1']);
-                // console.log('url : ',aPostedURL[aPostedURL.indexOf(url)],'id : ',aPostID[aPostedURL.indexOf(url)]);
                 console.log(arguments);
+                writeLog();
             });
         }
     });
@@ -310,32 +131,8 @@ function test2(page){
     });
 }
 
-// for(var i=10;i>0;i--){
-//     test2(i);
-// }
-
-function writeLog(){
-    var filedata = JSON.stringify(oPoosted);
-    fs.writeFile('./posted.enjoy', filedata, function(err) {
-        if(err) throw err;
-        console.log('Log File write completed');
-    });
-}
-
-setTimeout(function(){
-    test2(2);
-    test2(1);
-},1000);
-
-//1,2페이지 10분만다 파싱
-// setInterval(function(){test2(1);test2(2);},10*60*1000);
 
 
-//로그파일 5분 후 저장
-// setInterval(function(){writeLog();},5*60*1000);
-
-// test(20);
-// getVideo('http://tvko.us/tudou-%EC%8A%A4%ED%83%80%EB%89%B4%EC%8A%A4-140520/');
 
 app.use('/writeLog',function(req,res){
     var filedata = JSON.stringify(oPoosted);
