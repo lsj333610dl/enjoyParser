2d3895b587b5ad38808809881f6040047caafb80
lsj333610dl <lsj333610dl@naver.com>
2015-02-06 02:46:05 +0900

    add mongodb

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+
+
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
@@ -15,27 +17,34 @@ var url = require('url');
 
 var wordpress = require('wordpress');
 var wp = wordpress.createClient({
-    "url": 'http://enjoylimit.tv', 
-    "username": 'enjoylimit', 
-    "password": '963852dl' 
+    "url": 'http://enjoylimit.tv',
+    "username": 'enjoylimit',
+    "password": '963852dl'
 });
 
 var fs = require("fs");
 var jquery = fs.readFileSync("./jquery.js", "utf-8");
 
-var aPostedURL = [];
-var aPostID = [];
-var oPoosted = {};
 
-fs.readFile('./posted.enjoy', 'utf8', function(err, data) {
-  
-    if (!err) {
-        oPoosted = JSON.parse(data);
-        console.log("로그 로드완료.");
-        //처음 실행시 1페이지 파싱
-        doParse(1);
-    };
+//몽고DB
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/test');
+
+var db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function callback() {
+    console.log("db오픈 완료.");
+    //처음 실행시 1~2페이지 파싱
+    doParse(1);
+    doParse(2);
 });
+
+
+var VideoSchema = mongoose.Schema({
+    url: String
+});
+var Video = mongoose.model('videoModel', VideoSchema);
 
 
 
@@ -54,112 +63,116 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 
-function getVideo2(url){
+function getVideo2(url) {
+    Video.find({
+        url: url
+    }, function (err, res) {
 
-    if (oPoosted[url]) {
-//        console.log('중복자료');
-        return;
-    };
+        if (err)
+            throw err;
+        else if (res) {
+            console.log('디비에 있다');
+        } else {
+            console.log('디비중복아님');
+            var video = new Video({
+                url: url
+            });
+            video.save(function (err) {
+                if (err) {
+                    console.log(err);
+                    throw err;
+                }
+                console.log('db저장 성공');
+            });
 
-    jsdom.env({
-        url: url,
-        src: [jquery],
-        done: function (errors, window){
-            var $ = window.$;
-            var contentHtml = $('div.video').html();
-            var sTitle = $('.post-top').find('a:first').text();
 
-            wp.newPost({title:sTitle,status:'publish',content:contentHtml,author:1},function(){
-                oPoosted[url] = arguments['1'];
-//                console.log(arguments);
-                writeLog();
+            jsdom.env({
+                url: url,
+                src: [jquery],
+                done: function (errors, window) {
+                    var $ = window.$;
+                    var contentHtml = $('div.video').html();
+                    var sTitle = $('.post-top').find('a:first').text();
+
+                    wp.newPost({
+                        title: sTitle,
+                        status: 'publish',
+                        content: contentHtml,
+                        author: 1
+                    }, function () {
+                        //                        oPoosted[url] = arguments['1'];
+                        console.log(arguments);
+                        //                        writeLog();
+                    });
+                }
             });
         }
     });
-
 };
 
 
-function parseDrama(page){
-    console.log(page,'페이지 파싱');
-    var dramaUrl = 'http://julytv.com/category/drama/page/'+page;  
+function parseDrama(page) {
+    console.log(page, '페이지 파싱');
+    var dramaUrl = 'http://julytv.com/category/drama/page/' + page;
 
     jsdom.env({
-        url:dramaUrl,
-        src:[jquery],
-        done:function (errors, window) {
+        url: dramaUrl,
+        src: [jquery],
+        done: function (errors, window) {
             var $ = window.$;
             var contentHtml = $('div.col-left').html();
 
-            $('div.col-left').find('div.inside > a').each(function(){
+            $('div.col-left').find('div.inside > a').each(function () {
                 var postUrl = $(this).attr('href');
 
-                setTimeout(getVideo2(postUrl),5*1000);
+                setTimeout(getVideo2(postUrl), 5 * 1000);
             });
         }
-    });    
+    });
 }
 
-function parseShow(page){
+function parseShow(page) {
 
-    var showUrl = 'http://julytv.com/category/show/page/'+page;  
+    var showUrl = 'http://julytv.com/category/show/page/' + page;
 
     jsdom.env({
-        url:showUrl,
-        src:[jquery],
-        done:function (errors, window) {
+        url: showUrl,
+        src: [jquery],
+        done: function (errors, window) {
             var $ = window.$;
             var contentHtml = $('div.col-left').html();
 
-            $('div.col-left').find('div.inside > a').each(function(){
+            $('div.col-left').find('div.inside > a').each(function () {
                 var postUrl = $(this).attr('href');
-                
-                setTimeout(getVideo2(postUrl),5*1000);
-                
+
+                setTimeout(getVideo2(postUrl), 5 * 1000);
+
             });
         }
     });
 }
 
 
-function writeLog(){
-    var filedata = JSON.stringify(oPoosted);
-    fs.writeFile('./posted.enjoy', filedata, function(err) {
-        if(err) throw err;
-//        console.log('Log File write completed');
-    });
-}
 
-
-function doParse(parsePage){
-    setTimeout(function(){
+function doParse(parsePage) {
+    setTimeout(function () {
         parseDrama(parsePage);
         parseShow(parsePage);
-    },3*1000);
+    }, 3 * 1000);
 }
 
 
 //30분마다 반복
-setInterval(function(){
+setInterval(function () {
     console.log('반복 파싱시작');
-    
-    fs.readFile('./posted.enjoy', 'utf8', function(err, data) {
+    doParse(1);
+}, 30 * 60 * 1000);
 
-        if (!err) {
-            oPoosted = JSON.parse(data);
-            console.log("로그 로드완료.");
-
-            doParse(1);
-        };
-    });
-    
-    
-},30*60*1000);
 
 
 
 /// catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
     var err = new Error('Not Found');
     err.status = 404;
     next(err);
@@ -170,7 +183,7 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
+    app.use(function (err, req, res, next) {
         res.status(err.status || 500);
         res.render('error', {
             message: err.message,
@@ -181,7 +194,7 @@ if (app.get('env') === 'development') {
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
         message: err.message,
